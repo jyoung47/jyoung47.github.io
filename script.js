8197478fa6dfc913766964013269e26b94eefd7c
jyoung47 <80219870+jyoung47@users.noreply.github.com>
2023-08-14 23:41:38 -0400

    Update script.js
    Complete.

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
-let total = 0;
-let amount = 0;
-let lastSpinTime = 0;
+let total = 0; // Variable for grand total of cash winnings
+let amount = 0; // Variable for the wager (Spinning the wheel summons this)
+let lastSpinTime = 0; // Variable for start time when spin button is pressed (This is needed for time limit on spin button it prevents cheating) 
 const words = ["adventure", "technology", "happiness", "internet", "javascript", "html", "mountain", "pool", "king", "dreadlocks", "brilliant", "computer", "elephant", "fantastic", "gorgeous", "hospital", "hospital", "knowledge", "landscape", "national","overflow", "pictures", "quantity", "rainbow", "symphony", "treasure", "universe"];
-let randomWord = words[Math.floor(Math.random() * words.length)];
-const MAX_INCORRECT_GUESSES = 5;
-const wordDisplay = document.getElementById("word");
-const guessInput = document.getElementById("guessInput");
-const submitBtn = document.getElementById("submitLetter");
-const spinBtn = document.getElementById("spin");
-const guessesDisplay = document.getElementById("guesses");
-const amountDisplay = document.getElementById("amount-display");
-const totalDisplay = document.getElementById("total-display");
+//array of random puzzle words to solve
+let randomWord = words[Math.floor(Math.random() * words.length)];//random generator variable
+const MAX_INCORRECT_GUESSES = 5; // 5 incorrect guesses will make you lose variable
+const wordDisplay = document.getElementById("word"); //variable for puzzle board 
+const guessInput = document.getElementById("guessInput"); //value holder variable 
+const submitBtn = document.getElementById("submitLetter"); //submit button variable
+const spinBtn = document.getElementById("spin"); // spin button variable
+const guessesDisplay = document.getElementById("guesses"); // guess display
+const amountDisplay = document.getElementById("amount-display"); //constant variable for wager total
+const totalDisplay = document.getElementById("total-display"); //constant variable for grand total
 
-let currentWord = randomWord.split('');
-let guessedLetters = [];
-let incorrectGuesses = 0;
+let currentWord = randomWord.split('');//stores the selected random word as an array of letters (puzzle blanks on webpage)
+let guessedLetters = []; //empty array to store guessed letters
+let incorrectGuesses = 0; //starts at 0 wrong guesses
 
-wordDisplay.textContent = currentWord.map(() => '_').join(' ');
+wordDisplay.textContent = currentWord.map(() => '_').join(' '); //sets the content of the wordDisplay element to display underscores (_) based on the length of the currentWord.
 
-submitBtn.disabled = true;
+submitBtn.disabled = true; //disables submit button (until a spin)
 
 submitBtn.addEventListener("click", function() {
-    handleInput();
+    handleInput(); //When submit is pressed handleInput is called
 });
 
 spinBtn.addEventListener("click", function() {
   const currentTime = new Date().getTime();
 
-  if (currentTime - lastSpinTime >= 5000) {
+  if (currentTime - lastSpinTime >= 5000) {//event listener is added, this will enforce the 5 second rule for spinning
       lastSpinTime = currentTime; // Update the last spin time
 
       spin(); // Call the spin function
@@ -38,13 +39,13 @@ spinBtn.addEventListener("click", function() {
 
 
 function handleInput() {
-    guess();
+    guess(); //handleInput needs the guess function to help with displays
 }
 
-function updateWordDisplay() {
+function updateWordDisplay() { //main logic for game functionality. Updates the wordDisplay element's content by iterating over the currentWord array and replacing letters with underscores _ if they haven't been guessed yet.
   wordDisplay.textContent = currentWord.map(letter => guessedLetters.includes(letter) ? letter : '_').join(' ');
   if (checkWin(currentWord, guessedLetters)) {
-      alert(`Congratulations! You've won $${total}`);
+      alert(`Congratulations! You've won $${total}`); 
       resetGame(); 
       submitBtn.disabled = true;
       spinBtn.disabled = true;
@@ -53,6 +54,7 @@ function updateWordDisplay() {
 }
 
 function checkWin(currentWord, guessedLetters) {
+  //if letter is correct its true otherwise incorrect is false
     for (let i = 0; i < currentWord.length; i++) {
         if (!guessedLetters.includes(currentWord[i])) {
             return false;
@@ -118,8 +120,9 @@ function resetGame() {
   updateGuessesDisplay();
 }
 
-function spin() {
+function spin() {//mimics the wheel of fortune wheel to display a montary wager when spin is pressed
     amount = Math.floor(Math.random() * 100) + 10;
     amountDisplay.textContent = `$${amount}`; // Update amount display
     submitBtn.disabled = false;
 }
+
